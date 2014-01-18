896d7f3c5a69cda046b6bdac265882338aed14b5
Yorkie <l900422@vip.qq.com>
2014-01-19 05:04:42 +0800

    clean

--- build/Release/.deps/Release/net.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/net.node := ./gyp-mac-tool flock ./Release/linker.lock c++ -bundle -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release  -o Release/net.node Release/obj.target/net/src/net.o -undefined dynamic_lookup
