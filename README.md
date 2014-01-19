8fb7e475db14d8e002e6c8854956986e7c9f9b7f
Yorkie <l900422@vip.qq.com>
2014-01-19 21:43:12 +0800

    bump version 0.0.1

--- README.md
+++ README.md
@@ -3,8 +3,32 @@
 
 Alternative module to net, It's under being developed.
 
+
+### Installation
+
+```bash
+$ npm install net-alt
+```
+
+
+### Usage
+
+```js
+var socket = new netAlt.SimpleNet(143, '163.177.65.209');
+socket.connect(function(err, data) {
+  this.write('client says hello!\r\n');
+  this.end(); // close fd via this.
+});
+```
+
+
 ### Current comparison with 200 connections
 
 * net(NodeJS): 8.8M
 
 * SimpleNet: 7.2M
+
+
+### LICENSE
+
+MIT
---
+++ example.js
@@ -0,0 +1,21 @@
+
+var netAlt = require('./lib');
+
+for (var i=0; i<200; i++) {
+  var socket = new netAlt.SimpleNet(143, '163.177.65.209');
+  socket.count = 0;
+  socket.connect(function(err, data) {
+    console.log(data);
+    if (this.count++ < 1) {
+      this.write('a1 login l900422@vip.qq.com Lyz15285115151\r\n');
+    } else {
+      this.end();
+    }
+  });
+}
+
+var readline = require('readline');
+readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
